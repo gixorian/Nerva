98622daeea430052b5e75469db102fd6f8156e75
Goran <goranborovecki1@gmail.com>
2026-04-23 00:51:25 +0200

    Implemented stream based logging, switching out of file based logging. Implemented health checks within docker compose.

--- health_check.py
+++ health_check.py
@@ -2,22 +2,13 @@ import os
 import sys
 import logging
 
-LOG_DIR = "/app/logs"
-LOG_FILE = os.path.join(LOG_DIR, "health.log")
-
-if not os.path.exists(LOG_DIR):
-    os.makedirs(LOG_DIR, exist_ok=True)
-
-logger = logging.getLogger("HealthCheck")
-logger.setLevel(logging.INFO)
-
-file_handler = logging.FileHandler(LOG_FILE)
-
-formatter = logging.Formatter(
-    fmt="%(asctime)s - [%(levelname)s] -> %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)],
 )
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+
+logger = logging.getLogger(__name__)
 
 
 def check_path(path):
